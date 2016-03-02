8e31ea1e9c3b1cb5a06e4254c56d1ae948db15a1
Tim Wood <washwithcare@gmail.com>
2016-03-01 19:21:14 -0600

    Convert OffsetAt to an es6 module.

--- src/old.js
+++ src/old.js
@@ -49,29 +49,6 @@
 		Current Timezone
 	************************************/
 
-	function OffsetAt(at) {
-		var timeString = at.toTimeString();
-		var abbr = timeString.match(/\([a-z ]+\)/i);
-		if (abbr && abbr[0]) {
-			// 17:56:31 GMT-0600 (CST)
-			// 17:56:31 GMT-0600 (Central Standard Time)
-			abbr = abbr[0].match(/[A-Z]/g);
-			abbr = abbr ? abbr.join('') : undefined;
-		} else {
-			// 17:56:31 CST
-			// 17:56:31 GMT+0800 (台北標準時間)
-			abbr = timeString.match(/[A-Z]{3,5}/g);
-			abbr = abbr ? abbr[0] : undefined;
-		}
-
-		if (abbr === 'GMT') {
-			abbr = undefined;
-		}
-
-		this.at = +at;
-		this.abbr = abbr;
-		this.offset = at.getTimezoneOffset();
-	}
 
 	function ZoneScore(zone) {
 		this.zone = zone;
