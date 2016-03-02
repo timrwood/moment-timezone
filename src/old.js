9fd4f2e7bd5b8c2809390225672ed036030a7f7d
Tim Wood <washwithcare@gmail.com>
2016-03-02 10:14:23 -0600

    Extract findChange to an es6 module and write tests

--- src/old.js
+++ src/old.js
@@ -41,22 +41,6 @@
 		Current Timezone
 	************************************/
 
-
-	function findChange(low, high) {
-		var mid, diff;
-
-		while ((diff = ((high.at - low.at) / 12e4 | 0) * 6e4)) {
-			mid = new OffsetAt(new Date(low.at + diff));
-			if (mid.offset === low.offset) {
-				low = mid;
-			} else {
-				high = mid;
-			}
-		}
-
-		return low;
-	}
-
 	function userOffsets() {
 		var startYear = new Date().getFullYear() - 2,
 			last = new OffsetAt(new Date(startYear, 0, 1)),
