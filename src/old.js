795ab069a04b5a505d01c4db42870ce2bc56f7df
Tim Wood <washwithcare@gmail.com>
2016-03-02 11:46:01 -0600

    Extract userOffsets to an es6 module and write tests

--- src/old.js
+++ src/old.js
@@ -41,31 +41,6 @@
 		Current Timezone
 	************************************/
 
-	function userOffsets() {
-		var startYear = new Date().getFullYear() - 2,
-			last = new OffsetAt(new Date(startYear, 0, 1)),
-			offsets = [last],
-			change, next, i;
-
-		for (i = 1; i < 48; i++) {
-			next = new OffsetAt(new Date(startYear, i, 1));
-			if (next.offset !== last.offset) {
-				change = findChange(last, next);
-				offsets.push(change);
-				offsets.push(new OffsetAt(new Date(change.at + 6e4)));
-			}
-			last = next;
-		}
-
-		for (i = 0; i < 4; i++) {
-			offsets.push(new OffsetAt(new Date(startYear + i, 0, 1)));
-			offsets.push(new OffsetAt(new Date(startYear + i, 6, 1)));
-		}
-
-		return offsets;
-	}
-
-
 	function addToGuesses (name, offsets) {
 		var i, offset;
 		arrayToInt(offsets);
