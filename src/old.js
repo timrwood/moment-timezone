f09f0db2d8c62e501d63ec54c50a468d74a8f25f
Tim Wood <washwithcare@gmail.com>
2016-03-02 09:54:47 -0600

    Extract sortZoneScores to an es6 module and write tests

--- src/old.js
+++ src/old.js
@@ -81,15 +81,6 @@
 		return offsets;
 	}
 
-	function sortZoneScores (a, b) {
-		if (a.offsetScore !== b.offsetScore) {
-			return a.offsetScore - b.offsetScore;
-		}
-		if (a.abbrScore !== b.abbrScore) {
-			return a.abbrScore - b.abbrScore;
-		}
-		return b.zone.population - a.zone.population;
-	}
 
 	function addToGuesses (name, offsets) {
 		var i, offset;
